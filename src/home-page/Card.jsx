5b9fa43da986beb314c7967797ec53c1bc6fe0f0
BrunBruno <brunomaslon@gmail.com>
2023-06-23 01:45:49 +0200

    change texts

--- src/home-page/Card.jsx
+++ src/home-page/Card.jsx
@@ -2,36 +2,41 @@ import classes from "./Card.module.css";
 
 function Card(props) {
   function CardText() {
+    const containerClass =
+      props.index % 2 === 1
+        ? classes["card-text-bg-left"]
+        : classes["card-text-bg-right"];
     return (
-      <div className={classes["card-text"]}>
+      <div className={`${classes["card-text"]} ${containerClass}`}>
         <h2>
-          {props.index + 1}. {props.element.name}
+          <span>
+            {props.index + 1}. {props.element.name}
+          </span>
         </h2>
-        {props.element.text}
-        <p>
+        <h4>{props.element.maintext}</h4>
+        <span>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius illo
           libero deserunt veniam ut omnis cumque a? Mollitia, ratione! Ratione
           quod maxime animi aliquam? Aperiam mollitia ab fuga veritatis labore?
-        </p>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa natus
-          quidem assumenda? At magni nesciunt tempore atque commodi eos ex quo
-          dolore! Corrupti nulla ipsam sequi, impedit atque numquam minima!
-        </p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias
+          eligendi nam ducimus voluptates repellendus, possimus quasi
+          exercitationem, modi, deserunt enim porro quo unde optio? Autem nam
+          voluptates quidem sequi corrupti?
+        </span>
       </div>
     );
   }
 
-  function CardImage(imgProps) {
+  function CardImage() {
     const containerClass =
-      imgProps.pos === 0
+      props.index % 2 === 0
         ? classes["card-image-container-left"]
         : classes["card-image-container-right"];
     return (
       <div className={classes["card-image"]}>
         <div className={`${classes["card-image-container"]} ${containerClass}`}>
           <p>
-            <img src={props.element.image} />
+            <img src={props.element.image} alt={props.element.name} />
           </p>
           <p></p>
           <p></p>
@@ -46,11 +51,11 @@ function Card(props) {
         {props.index % 2 === 1 ? (
           <>
             <CardText />
-            <CardImage pos={props.index % 2} />
+            <CardImage />
           </>
         ) : (
           <>
-            <CardImage pos={props.index % 2} />
+            <CardImage />
             <CardText />
           </>
         )}
--- src/home-page/HomaPage.jsx
+++ src/home-page/HomaPage.jsx
@@ -4,20 +4,76 @@ import classes from "./HomePage.module.css";
 
 function HomePage() {
   const elements = [
-    { name: "Nagios", text: "Text", image: "./images/NagiosLogo.png" },
-    { name: "Zabbix", text: "Text", image: "./images/ZabbixLogo.png" },
-    { name: "Cacti", text: "Text", image: "./images/CactiLogo.png" },
-    { name: "Wireshark", text: "Text", image: "./images/WiresharkLogo.png" },
-    { name: "OpenNMS", text: "Text", image: "./images/OpenNMSLogo.png" },
-    { name: "Ansible", text: "Text", image: "./images/AnsibleLogo.png" },
-    { name: "Icinga", text: "Text", image: "./images/IcingaLogo.png" },
-    { name: "Nmap", text: "Text", image: "./images/NmapLogo.png" },
+    {
+      name: "Nagios",
+      maintext:
+        "Narzędzie do monitorowania sieci i systemów, które pozwala na śledzenie stanu urządzeń, usług i aplikacji w czasie rzeczywistym.",
+      text: "Text",
+      image: "./images/NagiosLogo.png",
+    },
+    {
+      name: "Zabbix",
+      maintext:
+        "Platforma monitorowania sieci i systemów, która oferuje zaawansowane funkcje analizy i wizualizacji danych, a także powiadomienia w czasie rzeczywistym.",
+      text: "Text",
+      image: "./images/ZabbixLogo.png",
+    },
+    {
+      name: "Cacti",
+      maintext:
+        "Narzędzie do wykresów i graficznej prezentacji danych o sieciach. Może być używane do monitorowania wydajności i zużycia zasobów.",
+      text: "Text",
+      image: "./images/CactiLogo.png",
+    },
+    {
+      name: "Wireshark",
+      maintext:
+        "Narzędzie analizy ruchu sieciowego, które umożliwia przechwytywanie i analizę pakietów danych w czasie rzeczywistym. Jest szczególnie przydatne w rozwiązywaniu problemów z siecią.",
+      text: "Text",
+      image: "./images/WiresharkLogo.png",
+    },
+    {
+      name: "OpenNMS",
+      maintext:
+        "Platforma monitorowania sieci, która oferuje wiele zaawansowanych funkcji, takich jak wykrywanie i zarządzanie urządzeniami sieciowymi, monitorowanie usług i alarmowanie.",
+      text: "Text",
+      image: "./images/OpenNMSLogo.png",
+    },
+    {
+      name: "Ansible",
+      maintext:
+        "Narzędzie do automatyzacji zarządzania konfiguracją i wdrażania aplikacji. Umożliwia administratorom zdalne zarządzanie systemami i aplikacjami.",
+      text: "Text",
+      image: "./images/AnsibleLogo.png",
+    },
+    {
+      name: "Icinga",
+      maintext:
+        "Narzędzie monitorujące, które pozwala na monitorowanie infrastruktury IT, usług i aplikacji. Oferuje elastyczną konfigurację i rozszerzalność.",
+      text: "Text",
+      image: "./images/IcingaLogo.png",
+    },
+    {
+      name: "Nmap",
+      maintext:
+        "Narzędzie do skanowania sieci, które pozwala na badanie dostępności i bezpieczeństwa hostów w sieci. Może być używane do identyfikacji otwartych portów, mapowania sieci i analizy zabezpieczeń.",
+      text: "Text",
+      image: "./images/NmapLogo.png",
+    },
     {
       name: "Apache JMeter",
+      maintext:
+        "Narzędzie do testowania wydajności aplikacji webowych. Umożliwia symulację obciążenia na serwerze, monitorowanie wydajności i analizę wyników.",
       text: "Text",
       image: "./images/ApacheJMeterLogo.png",
     },
-    { name: "GLPI", text: "Text", image: "./images/GLPILogo.png" },
+    {
+      name: "GLPI",
+      maintext:
+        "System do zarządzania bazą wiedzy i zasobami IT. Oferuje możliwość śledzenia urządzeń, zarządzania zgłoszeniami użytkowników, monitorowania aktywów i wiele innych funkcji.",
+      text: "Text",
+      image: "./images/GLPILogo.png",
+    },
   ];
   return (
     <>
